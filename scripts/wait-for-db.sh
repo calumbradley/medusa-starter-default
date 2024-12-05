2a68f35dafb5a7d9245a084183f35ba71406c0a0
Calum Bradley <calumbradley@icloud.com>
2024-12-05 11:46:46 +0000

    run app at the last stage

--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -2,12 +2,17 @@
 
 host="$1"
 shift
-cmd="$@"
+cmd1="$1"
+shift
+cmd2="$@"
 
 until pg_isready -h "$host" -p 5432; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd
+>&2 echo "Postgres is up - executing cmd1"
+$cmd1
+
+>&2 echo "Running npm run dev"
+$cmd2
