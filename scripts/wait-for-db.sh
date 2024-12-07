831cf7adfa5cf6fa98ccb3530053eaad4622f069
Calum Bradley <calumbradley@icloud.com>
2024-12-07 14:32:50 +0000

    Improve logging in wait-for-db script to clarify command execution

--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -11,8 +11,8 @@ until pg_isready -h "$host" -p 5432; do
   sleep 1
 done
 
->&2 echo "Postgres is up - executing cmd1"
+>&2 echo "Executing: $cmd1"
 $cmd1
 
->&2 echo "Running npm run dev"
+>&2 echo "Executing: $cmd2"
 $cmd2
